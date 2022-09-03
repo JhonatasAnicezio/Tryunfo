7e2b4ec07749eaa93e7f49183038beb313f7ec6e
Jhonatas <jhonatasanic@hotmail.com>
2022-09-03 09:56:46 -0300

    feat(components/forms): create component Select

---
+++ src/components/forms/Select.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Select({ text, name, func, value, placeholder }) {
+  return (
+    <div>
+      <label htmlFor={ name }>{ text }</label>
+      <select name={ name } id={ name } onChange={ func }>
+        <option disabled select>{ placeholder }</option>
+        { value.map((val, index) => 
+          <option
+            value={ val }
+            key={ index }
+          >
+            { val }
+          </option>
+        ) }
+      </select>
+    </div>
+  );
+}
+
+Select.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
+
+export default Select;
