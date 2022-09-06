8677e3d0a8b817e136b3a1eff0de16c6b2c26ce8
Jhonatas <jhonatasanic@hotmail.com>
2022-09-06 19:00:45 -0300

    feat(App): add handleOnChange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from './components/Form';
 
-function App() {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName:'',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
+  }
+
+  handleOnChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name] : value });
+    console.log( this.state );
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          values={ this.state }
+          onInputChange={ this.handleOnChange }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
