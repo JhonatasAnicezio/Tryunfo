463a57008b3d21aac6376ac3f0d167f5f6bf7eb5
Jhonatas <jhonatasanic@hotmail.com>
2022-09-11 11:30:32 -0300

    feat(services): create fake backend for Cards

---
+++ src/services.js
@@ -0,0 +1,88 @@
+const Dany_Description = `"O mundo não via um dragão havia séculos,
+ até os meus filhos nascerem.Os dothraki não tinham cruzado o mar.
+  Nenhum mar. Eles cruzaram por mim. Eu nasci para comandar os Sete Reinos.
+  E vou comandar."`;
+const Dany_Image = 'http://pm1.narvii.com/6577/926f7f54e99d18e7e0b6e4c1de2b725060540212_00.jpg';
+
+const Jon_Description = '"O mundo que precisamos é um mundo de misericórdia, tem de ser."';
+const Jon_Image = 'https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2022/06/17/1748692111-game-of-thrones-kit-harington-jon-snow.jpg';
+
+const Tyrion_Description = `"Tyrion: "Eu sou culpado de um crime muito mais monstruoso. Eu sou culpado de ser um anão!"
+Tywin: "Você não está em julgamento por ser um anão."
+Tyrion: "Oh, sim eu estou! Sou julgado por isso em toda a minha vida!"`;
+const Tyrion_Image = 'https://i.scdn.co/image/ab67706c0000bebb9e4e96e77aecc3bbda585739';
+
+const Jorah_Description = `Viserys: "Lealdade não significa nada para você?"
+Jorah: "Significa tudo para mim."`;
+const Jorah_Image = 'https://i.pinimg.com/564x/57/3e/e0/573ee0a623bf54a6453ed7cc55d566a1.jpg';
+
+const Arya_Description = `"A garota é Arya Stark de Winterfell, e eu vou para casa."
+―Arya Stark, para um homem chamado Jaqen H'ghar`;
+const Arya_Image = 'https://www.lavenir.net/resizer/s1qYJ9GfOByYMTwM20TEjKvSn-8=/768x512/filters:focal(368.5x254:378.5x244)/cloudfront-eu-central-1.images.arcpublishing.com/ipmgroup/SEVZTS3V4VCOFP52MVUXUBJR3E.jpg';
+
+const Jaime_Description = `"Tantos juramentos... eles te fazem jurar e jurar. Defenda o rei. Obedeça o rei. Guarde seus segredos.
+ Faça seu trabalho. Sua vida pela dele. Mas obedeça seu pai... É muita coisa. Não importa o que você faz, está quebrando um juramento ou outro."`;
+const Jaime_Image = 'https://i.pinimg.com/736x/05/f7/20/05f7201a1f966461c7bbdaf8e5a0c35b--jaime-lannister.jpg';
+
+export const game_of_Cards = [
+  {
+    cardName:'Daenerys Targaryen',
+    cardDescription: Dany_Description,
+    cardAttr1: '90',
+    cardAttr2: '90',
+    cardAttr3: '20',
+    cardImage: Dany_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+  {
+    cardName:'Jon Snow',
+    cardDescription: Jon_Description,
+    cardAttr1: '90',
+    cardAttr2: '20',
+    cardAttr3: '90',
+    cardImage: Jon_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+  {
+    cardName:'Tyrion Lannister',
+    cardDescription: Tyrion_Description,
+    cardAttr1: '20',
+    cardAttr2: '90',
+    cardAttr3: '90',
+    cardImage: Tyrion_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+  {
+    cardName:'Sor Jorah Mormont',
+    cardDescription: Jorah_Description,
+    cardAttr1: '20',
+    cardAttr2: '90',
+    cardAttr3: '90',
+    cardImage: Jorah_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+  {
+    cardName:'Arya Stark',
+    cardDescription: Arya_Description,
+    cardAttr1: '20',
+    cardAttr2: '90',
+    cardAttr3: '90',
+    cardImage: Arya_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+  {
+    cardName:'Sor Jaime Lannister',
+    cardDescription: Jaime_Description,
+    cardAttr1: '20',
+    cardAttr2: '90',
+    cardAttr3: '90',
+    cardImage: Jaime_Image,
+    cardRare: 'muito raro',
+    cardTrunfo: false,
+  },
+];
