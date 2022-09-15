dac7c248482f65085d9edc99e92934d566b816be
Jhonatas <jhonatasanic@hotmail.com>
2022-09-15 12:50:57 -0300

    fix(services): fix descriptions length

--- src/services.js
+++ src/services.js
@@ -1,62 +1,47 @@
-const Dany_Description = `"O mundo não via um dragão havia séculos,
- até os meus filhos nascerem.Os dothraki não tinham cruzado o mar.
-  Nenhum mar. Eles cruzaram por mim. Eu nasci para comandar os Sete Reinos.
-  E vou comandar."`;
-const Dany_Image = 'http://pm1.narvii.com/6577/926f7f54e99d18e7e0b6e4c1de2b725060540212_00.jpg';
-
+const Dany_Description = `"O mundo não via um dragão havia séculos, até os meus filhos nascerem.
+ Os dothraki não tinham cruzado o mar. Nenhum mar. Eles cruzaram por mim. Eu nasci para comandar os Sete Reinos."`;
 const Jon_Description = '"O mundo que precisamos é um mundo de misericórdia, tem de ser."';
-const Jon_Image = 'https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2022/06/17/1748692111-game-of-thrones-kit-harington-jon-snow.jpg';
-
-const Tyrion_Description = `"Tyrion: "Eu sou culpado de um crime muito mais monstruoso. Eu sou culpado de ser um anão!"
+const Tyrion_Description = `"Tyrion: "Eu sou culpado de ser um anão!"
 Tywin: "Você não está em julgamento por ser um anão."
 Tyrion: "Oh, sim eu estou! Sou julgado por isso em toda a minha vida!"`;
-const Tyrion_Image = 'https://i.scdn.co/image/ab67706c0000bebb9e4e96e77aecc3bbda585739';
-
 const Jorah_Description = `Viserys: "Lealdade não significa nada para você?"
 Jorah: "Significa tudo para mim."`;
-const Jorah_Image = 'https://i.pinimg.com/564x/57/3e/e0/573ee0a623bf54a6453ed7cc55d566a1.jpg';
-
 const Arya_Description = `"A garota é Arya Stark de Winterfell, e eu vou para casa."
 ―Arya Stark, para um homem chamado Jaqen H'ghar`;
-const Arya_Image = 'https://www.lavenir.net/resizer/s1qYJ9GfOByYMTwM20TEjKvSn-8=/768x512/filters:focal(368.5x254:378.5x244)/cloudfront-eu-central-1.images.arcpublishing.com/ipmgroup/SEVZTS3V4VCOFP52MVUXUBJR3E.jpg';
-
-const Jaime_Description = `"Tantos juramentos... eles te fazem jurar e jurar. Defenda o rei. Obedeça o rei. Guarde seus segredos.
- Faça seu trabalho. Sua vida pela dele. Mas obedeça seu pai... É muita coisa. Não importa o que você faz, está quebrando um juramento ou outro."`;
-const Jaime_Image = 'https://i.pinimg.com/736x/05/f7/20/05f7201a1f966461c7bbdaf8e5a0c35b--jaime-lannister.jpg';
-
+const Jaime_Description = `"...eles te fazem jurar e jurar. Defenda o rei. Obedeça o rei. Guarde seus segredos.
+Faça seu trabalho. [...] É muita coisa. Não importa o que você faz, está quebrando um juramento ou outro."`; 
 const Sansa_Description = `"Eu fiz o que eu fiz para minha sobrevivência, minha senhora. Mas eu sou uma Stark, sempre serei uma Stark."
 ―Sansa Stark, para Lady Lyanna Mormont`;
-const Sansa_Image = 'https://assets.reedpopcdn.com/6f69d026_1b4b_4f2c_a7aa_68049fb46ce7_sansa_tyrion_still_married_game_thrones.jpg/BROK/thumbnail/1200x1200/quality/100/6f69d026_1b4b_4f2c_a7aa_68049fb46ce7_sansa_tyrion_still_married_game_thrones.jpg';
-
 const Bronn_Description = `Lysa Arryn: "Você não luta com honra!"
 Bronn: "Não ... ele luta"
 ―Lysa Arryn e Bronn, indicando Sor Vardis Egen, a quem ele acabou de matar em combate único.`;
-const Bronn_Image = 'https://i.pinimg.com/564x/87/11/b1/8711b1bf649f638bce082f6eee0c282d--jerome-flynn-game-of-thornes.jpg';
-
-const Barristan_Description = `"Gastei os meus anos lutando por péssimos reis. [...] Um homem com honra mantém seus votos, mesmo se ele está servindo a um bêbado ou um louco. Apenas uma vez na vida, antes que ela acabe, quero saber como é servir com orgulho, lutar por alguém em quem acredito."
+const Barristan_Description = `"Gastei os meus anos lutando por péssimos reis. [...] Um homem com honra mantém seus votos, mesmo se ele está servindo a um bêbado ou um louco..."
 ―Barristan Selmy para Jorah Mormont`;
-const Barristan_Image = 'https://topicimages.mrowl.com/large/chaseosburn/chaos_is_a_ladder/actors/ian_mcelhinney__barristan_selmy_1.jpg';
-
 const Bran_Description = '"Não devia me invejar. A maior parte do tempo eu vivo no passado."';
-const Bran_Image = 'https://s.yimg.com/ny/api/res/1.2/KQIhAOQHuIL8W.yMccmMaA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ4MA--/https://s.yimg.com/uu/api/res/1.2/jQO1Jp26xlGkDsetvHPcnw--~B/aD05MDA7dz0xMjAwO2FwcGlkPXl0YWNoeW9u/https://img.huffingtonpost.com/asset/5cc351d3240000680038b7f7.jpeg';
-
 const Brienne_Description = `"Em toda minha vida homens como você me ridicularizavam, e em toda a minha vida eu venho derrubando homens como você na poeira."
 ―Brienne de Tarth para Jaime Lannister`;
-const Brienne_Image = 'https://conteudo.imguol.com.br/c/entretenimento/f2/2019/03/28/brienne-gwendoline-christie-em-cena-da-oitava-temporada-de-game-of-thrones-1553792038971_v2_1x1.jpg';
-
-const Verme_Description = `"'Verme Cinzento' me dá um orgulho. É um nome de sorte. O nome com o qual nasci foi amaldiçoado. Esse era o nome que ele tinha quando foi levado como escravo. Mas Verme Cinzento é o nome que este teve no dia em que Daenerys me libertou "
+const Verme_Description = `"'Verme Cinzento' me dá um orgulho. É um nome de sorte. O nome com o qual nasci foi amaldiçoado.  Mas Verme Cinzento é o nome que este teve no dia em que Daenerys me libertou "
 ―Verme Cinzento`;
-const Verme_Image = 'https://figurinepop.com/public/greyworm1_1.jpg';
-
 const Daario_Description = 'Originalmente um tenente dos Segundos Filhos, ele assume a companhia depois de matar os seus superiores Mero e Prendahl na Ghezn e depois ele se alinha com Daenerys Targaryen.';
-const Daario_Image='https://celebrity.fm/wp-content/uploads/2021/08/Does-Daario-Naharis-die-732x549.jpeg';
-
 const Podrick_Description=`"Nunca existiu um escudeiro tão leal."
 ―Tyrion Lannister para Podrick`;
-const Podrick_Image='http://esq.h-cdn.co/assets/17/32/1280x1280/square-1502197596-9ee1b5f5648fb2f38d720be85218587ec0dd27d95b1e373bbf4069c64b54bb98e11aa02cb9d8ae59042b92149c81981f.jpg';
-
 const Gendry_Description=`"Meu nome é Gendry, Vossa Graça. Eu sou filho de Robert Baratheon. Filho bastardo."
 ―Gendry se apresenta a Jon Snow`;
+
+const Dany_Image = 'http://pm1.narvii.com/6577/926f7f54e99d18e7e0b6e4c1de2b725060540212_00.jpg';
+const Jon_Image = 'https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2022/06/17/1748692111-game-of-thrones-kit-harington-jon-snow.jpg';
+const Tyrion_Image = 'https://i.scdn.co/image/ab67706c0000bebb9e4e96e77aecc3bbda585739';
+const Jorah_Image = 'https://i.pinimg.com/564x/57/3e/e0/573ee0a623bf54a6453ed7cc55d566a1.jpg';
+const Arya_Image = 'https://www.lavenir.net/resizer/s1qYJ9GfOByYMTwM20TEjKvSn-8=/768x512/filters:focal(368.5x254:378.5x244)/cloudfront-eu-central-1.images.arcpublishing.com/ipmgroup/SEVZTS3V4VCOFP52MVUXUBJR3E.jpg';
+const Jaime_Image = 'https://i.pinimg.com/736x/05/f7/20/05f7201a1f966461c7bbdaf8e5a0c35b--jaime-lannister.jpg';
+const Sansa_Image = 'https://assets.reedpopcdn.com/6f69d026_1b4b_4f2c_a7aa_68049fb46ce7_sansa_tyrion_still_married_game_thrones.jpg/BROK/thumbnail/1200x1200/quality/100/6f69d026_1b4b_4f2c_a7aa_68049fb46ce7_sansa_tyrion_still_married_game_thrones.jpg';
+const Bronn_Image = 'https://i.pinimg.com/564x/87/11/b1/8711b1bf649f638bce082f6eee0c282d--jerome-flynn-game-of-thornes.jpg';
+const Barristan_Image = 'https://topicimages.mrowl.com/large/chaseosburn/chaos_is_a_ladder/actors/ian_mcelhinney__barristan_selmy_1.jpg';
+const Bran_Image = 'https://s.yimg.com/ny/api/res/1.2/KQIhAOQHuIL8W.yMccmMaA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ4MA--/https://s.yimg.com/uu/api/res/1.2/jQO1Jp26xlGkDsetvHPcnw--~B/aD05MDA7dz0xMjAwO2FwcGlkPXl0YWNoeW9u/https://img.huffingtonpost.com/asset/5cc351d3240000680038b7f7.jpeg';
+const Brienne_Image = 'https://conteudo.imguol.com.br/c/entretenimento/f2/2019/03/28/brienne-gwendoline-christie-em-cena-da-oitava-temporada-de-game-of-thrones-1553792038971_v2_1x1.jpg';
+const Verme_Image = 'https://figurinepop.com/public/greyworm1_1.jpg';
+const Daario_Image='https://celebrity.fm/wp-content/uploads/2021/08/Does-Daario-Naharis-die-732x549.jpeg';
+const Podrick_Image='http://esq.h-cdn.co/assets/17/32/1280x1280/square-1502197596-9ee1b5f5648fb2f38d720be85218587ec0dd27d95b1e373bbf4069c64b54bb98e11aa02cb9d8ae59042b92149c81981f.jpg';
 const Gendry_Image='https://s-media-cache-ak0.pinimg.com/736x/50/a3/65/50a365cd46d09771f193922ef8bf1939.jpg';
 
 export const game_of_Cards = [
